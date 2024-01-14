e3f31e3eec520e13dd5749da5ae2c7aa4377465c
Kamil Mankowski (kam193) <me@kam193.eu>
2024-01-14 01:41:44 +0100

    Initial versio of Comodo AV service

---
+++ comodo-av-service/README.md
@@ -0,0 +1,7 @@
+# Comodo-Antivirus
+
+https://www.comodo.com/home/internet-security/antivirus-for-linux.php
+
+The AV itself looks discontinued, but it shares definitions with other products, so it's still useful.
+
+Pay attention to the license - in container in "/opt/COMODO/doc/eula_free.txt"
---
+++ comodo-av-service/service/updater.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import tempfile
+
+from assemblyline_v4_service.updater.updater import ServiceUpdater
+
+
+class AssemblylineServiceUpdater(ServiceUpdater):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def import_update(self, files_sha256, client, source, default_classification) -> None:
+        self.log.info(str(files_sha256))
+        output_dir = os.path.join(self.latest_updates_dir, source)
+        os.makedirs(os.path.join(self.latest_updates_dir, source), exist_ok=True)
+        for file, _ in files_sha256:
+            if not file.endswith("bases.cav"):
+                self.log.warning("Skipping %s because it is not a bases.cav file", file)
+                continue
+            self.log.debug("Copying %s to %s", file, output_dir)
+            shutil.copy(file, output_dir)
+
+    def prepare_output_directory(self) -> str:
+        tempdir = tempfile.mkdtemp()
+        shutil.copytree(self.latest_updates_dir, tempdir, dirs_exist_ok=True)
+        return tempdir
+
+
+if __name__ == "__main__":
+    with AssemblylineServiceUpdater(default_pattern=".*") as server:
+        server.serve_forever()
