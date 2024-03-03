eba9d5584741cee23a575b1c74512788e20031a0
Kamil Mankowski (kam193) <me@kam193.eu>
2024-03-03 19:26:26 +0100

    Extract CIRCL Hashlookup

--- hashlookup/service/al_run.py
+++ hashlookup/service/al_run.py
@@ -1,5 +1,6 @@
 import functools
 import json
+import logging
 import os
 
 import dns.exception
@@ -18,14 +19,12 @@ from assemblyline_v4_service.common.result import (
 )
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from abc import ABC, abstractmethod
 
 REDIS_SERVER = os.getenv("hashlookup_redis_host", "hashlookup_redis")
 REDIS_TTL = 3600 * 24
 
-HASHLOOKUP_SERVER = "dns.hashlookup.circl.lu"
-HASHLOOKUP_API = "https://hashlookup.circl.lu/"
 
-MAX_PARENTS = 10
 NX_SENTINEL = "NX"
 RETRIES = 3
 TIMEOUT = 10
@@ -68,6 +67,69 @@ def retry(exceptions):
     return decorator
 
 
+class CachedLookup(ABC):
+    CACHE_TEMPLATE = "hash:{}"
+
+    def __init__(
+        self,
+        session: requests.Session,
+        redis_client: redis.Redis,
+        resolver: dns.resolver.Resolver,
+        logger: logging.Logger,
+    ):
+        self._session = session
+        self.redis_client = redis_client
+        self._resolver = resolver
+        self.log = logger
+
+    @abstractmethod
+    def _lookup(self, sha1: str) -> dict:
+        raise NotImplementedError
+
+    @retry((redis.RedisError, dns.exception.Timeout))
+    def lookup_sha1(self, sha1: str):
+        response = None
+        if cached := self.redis_client.get(self.CACHE_TEMPLATE.format(sha1)):
+            response = cached.decode("utf-8")
+            return json.loads(response) if response != NX_SENTINEL else None
+
+        try:
+            data = self._lookup(sha1)
+            self.redis_client.set(self.CACHE_TEMPLATE.format(sha1), json.dumps(data), ex=REDIS_TTL)
+            return data
+        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+            response = NX_SENTINEL
+            self.redis_client.set(self.CACHE_TEMPLATE.format(sha1), NX_SENTINEL, ex=REDIS_TTL)
+
+        return None
+
+
+class CIRCLHashlookup(CachedLookup):
+    HASHLOOKUP_SERVER = "dns.hashlookup.circl.lu"
+    HASHLOOKUP_API = "https://hashlookup.circl.lu/"
+    MAX_PARENTS = 10
+    CACHE_TEMPLATE = "circl:{}"
+
+    def _get_details(self, sha1: str):
+        response = self._session.get(f"{self.HASHLOOKUP_API}/lookup/sha1/{sha1}").json()
+        data = {
+            "source": response.get("source", ""),
+            "name": response.get("FileName", ""),
+            "db": response.get("db", ""),
+            "trust": response.get("hashlookup:trust", 50),
+            "parents": response.get("parents", [])[: self.MAX_PARENTS],
+            "product": response.get("ProductCode", {}),
+        }
+        return data
+
+    def _lookup(self, sha1: str):
+        dns_answer = self._resolver.resolve(f"{sha1.lower()}.{self.HASHLOOKUP_SERVER}", "TXT")
+        dns_answer = dns_answer[0]
+        self.log.debug("lookup_sha1(%s) = %s", sha1, dns_answer)
+        data = self._get_details(sha1)
+        return data
+
+
 class AssemblylineService(ServiceBase):
     def __init__(self, config=None):
         super().__init__(config)
@@ -78,6 +140,9 @@ class AssemblylineService(ServiceBase):
         self.break_on_known = self.config.get("stop_scan_for_well_known", True)
         self.break_deep_scan = self.config.get("stop_deep_scan_for_well_known", False)
 
+        self.use_circl = self.config.get("use_circl_hashlookup", True)
+        self.use_cymru = self.config.get("use_cymru_malware_hash", True)
+
         self.safe_level = self.config.get("safe_level", TRUST_SAFE_LEVEL)
         self.unsafe_level = self.config.get("unsafe_level", TRUST_UNSAFE_LEVEL)
 
@@ -86,6 +151,10 @@ class AssemblylineService(ServiceBase):
             self._resolver.nameservers = [self.dns_server]
 
         self._session = create_session(RETRIES, TIMEOUT)
+
+        self.circl_hashlookup = CIRCLHashlookup(
+            self._session, self.redis_client, self._resolver, self.log
+        )
 
     @property
     def redis_client(self):
@@ -96,38 +165,6 @@ class AssemblylineService(ServiceBase):
 
         return self._redis
 
-    def _get_details(self, sha1: str):
-        response = self._session.get(f"{HASHLOOKUP_API}/lookup/sha1/{sha1}").json()
-        data = {
-            "source": response.get("source", ""),
-            "name": response.get("FileName", ""),
-            "db": response.get("db", ""),
-            "trust": response.get("hashlookup:trust", 50),
-            "parents": response.get("parents", [])[:MAX_PARENTS],
-            "product": response.get("ProductCode", {}),
-        }
-        return data
-
-    @retry((redis.RedisError, dns.exception.Timeout))
-    def lookup_sha1(self, sha1: str):
-        response = None
-        if cached := self.redis_client.get(sha1):
-            response = cached.decode("utf-8")
-            return json.loads(response) if response != NX_SENTINEL else None
-
-        try:
-            dns_answer = self._resolver.resolve(f"{sha1.lower()}.{HASHLOOKUP_SERVER}", "TXT")
-            dns_answer = dns_answer[0]
-            self.log.debug("lookup_sha1(%s) = %s", sha1, dns_answer)
-            data = self._get_details(sha1)
-            self.redis_client.set(sha1, json.dumps(data), ex=REDIS_TTL)
-            return data
-        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
-            response = NX_SENTINEL
-            self.redis_client.set(sha1, NX_SENTINEL, ex=REDIS_TTL)
-
-        return None
-
     def start(self):
         self.log.info(f"start() from {self.service_attributes.name} service called")
         self._load_config()
@@ -135,16 +172,27 @@ class AssemblylineService(ServiceBase):
         self.log.info(f"{self.service_attributes.name} service started")
 
     def execute(self, request: ServiceRequest) -> None:
-        info = self.lookup_sha1(request.sha1)
-        if not info:
-            request.result = Result()
-            return
+        request.result = Result()
 
-        main_section = ResultMultiSection("Hashlookup Results")
+        if self.use_circl:
+            circl_section = self._check_circl(request)
+            if circl_section:
+                request.result.add_section(circl_section)
+
+    def _check_circl(self, request: ServiceRequest):
+        info = self.circl_hashlookup.lookup_sha1(request.sha1)
+        if not info:
+            return None
+
+        main_section = ResultMultiSection("CIRCL Hashlookup Results")
         description = TextSectionBody()
         description.add_line(f"Known under name: \"{info['name']}\"")
         description.add_line(f"by source {info['source']} {info['db'] if info['db'] else ''}")
         description.add_line(f"Trust level: {info['trust']}")
+        description.add_line("")
+        description.add_line(
+            "Data sourced from CIRCL Hashlookup service, https://www.circl.lu/services/hashlookup/"
+        )
         main_section.add_section_part(description)
 
         if info.get("parents"):
@@ -168,7 +216,4 @@ class AssemblylineService(ServiceBase):
                 request.drop()
             elif self.break_deep_scan and request.deep_scan:
                 request.drop()
-
-        result = Result()
-        result.add_section(main_section)
-        request.result = result
+        return main_section
