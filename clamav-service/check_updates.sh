c4389db28ad6574fd46b22fa6006efc3706278c0
Kamil Mankowski (kam193) <me@kam193.eu>
2024-10-11 12:18:43 +0200

    Update check script

--- clamav-service/check_updates.sh
+++ clamav-service/check_updates.sh
@@ -2,18 +2,21 @@
 
 # This script checks if there is a new version of clamav available
 
+BASE_TAG=4.5.0.stable
 TAG=$(cat VERSION)
 REGISTRY=packages.lab.kam193.eu:9443
 
-docker build -t kam193/assemblyline-service-clamav-check:$TAG --build-arg REGISTRY=$REGISTRY --build-arg TAG=$TAG -f Dockerfile.check_updates .
-CHECK=`docker run --rm --name ClamAVServiceCheck kam193/assemblyline-service-clamav-check:$TAG 2>&1 | grep clamav-daemon`
+# make build
+CHECK=`docker run --rm -u root --name ClamAVServiceCheck ghcr.io/kam193/assemblyline-service-clamav:${BASE_TAG}${TAG} bash -c "apt-get update && apt-get upgrade --dry-run" | grep clamav-daemon`
+
+echo "${CHECK}"
 
 if [ "${CHECK}" ]; then
     echo "Update available $CHECK"
     # Force recreation of layer by changing the comment
-    VERSION_MARK="Expected clamd: $CHECK"
-    sed -i "s#Expected clamd: .*#$VERSION_MARK#g" Dockerfile
-    make push
+    # VERSION_MARK="Expected clamd: $CHECK"
+    # sed -i "s#Expected clamd: .*#$VERSION_MARK#g" Dockerfile
+    # make push
 else
     echo "No update available"
 fi
