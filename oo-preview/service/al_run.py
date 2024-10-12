ba107ffd83bb5549857445d278969d08f5034896
Kamil Mankowski (kam193) <me@kam193.eu>
2024-10-12 20:32:14 +0200

    Fix handling not generated preview and sorting

--- oo-preview/service/al_run.py
+++ oo-preview/service/al_run.py
@@ -31,10 +31,10 @@ class AssemblylineService(ServiceBase):
         except KeyError:
             ext = "." + request.file_type.split("/")[-1]
 
-        input_path = os.path.join(self.working_directory, f"builder_input{ext}")
-        if os.path.exists(input_path):
-            os.remove(input_path)
-        os.symlink(request.file_path, input_path)
+        self._input_path = os.path.join(self.working_directory, f"builder_input{ext}")
+        if os.path.exists(self._input_path):
+            os.remove(self._input_path)
+        os.symlink(request.file_path, self._input_path)
 
         output_file = "image.png"
         only_first = "true"
@@ -43,26 +43,33 @@ class AssemblylineService(ServiceBase):
             output_file = "images.zip"
             only_first = "false"
 
-        output_path = os.path.join(self.working_directory, output_file)
+        self._output_path = os.path.join(self.working_directory, output_file)
 
         builder = docbuilder.CDocBuilder()
-        builder.OpenFile(input_path, "")
+        builder.OpenFile(self._input_path, "")
         builder.SaveFile(
             "image",
-            output_path,
-            f"<m_oThumbnail><format>4</format><aspect>1</aspect><first>{only_first}</first><width>1500</width><height>1500</height></m_oThumbnail>",
+            self._output_path,
+            f"<m_oThumbnail><format>4</format><aspect>1</aspect><first>{only_first}</first><width>2000</width><height>2000</height></m_oThumbnail>",
         )
         builder.CloseFile()
+
+        if not os.path.exists(self._output_path):
+            self.log.info(
+                "No preview generated - file type %s may not be supported", request.file_type
+            )
+            return
 
         request.set_service_context(f"OnlyOffice {builder.GetVersion().decode()}")
         section = ResultImageSection(request, "Document preview")
         if not request.get_param("preview_all_pages"):
-            section.add_image(output_path, "First page", "Preview of the first page")
+            section.add_image(self._output_path, "First page", "Preview of the first page")
         else:
             page = 1
-            with zipfile.ZipFile(output_path, "r") as zip_ref:
+            with zipfile.ZipFile(self._output_path, "r") as zip_ref:
                 total_pages = len(zip_ref.filelist)
-                for file in zip_ref.filelist:
+                sorted_files = sorted(zip_ref.filelist, key=lambda x: x.filename)
+                for file in sorted_files:
                     zip_ref.extract(file, self.working_directory)
                     section.add_image(
                         os.path.join(self.working_directory, file.filename),
@@ -74,6 +81,8 @@ class AssemblylineService(ServiceBase):
         section.promote_as_screenshot()
         result.add_section(section)
 
+    def _cleanup(self):
         with suppress(Exception):
-            os.remove(input_path)
-            os.remove(output_path)
+            os.remove(self._input_path)
+            os.remove(self._output_path)
+        super()._cleanup()
